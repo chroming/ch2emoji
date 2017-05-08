1eee77ea35f2d2fe9f2acba7d428b2fbe4759819
chroming <guangzhishang@gmail.com>
2017-05-09 00:28:04 +0800

    通过emoji 全部中文字典随机选择 emoji

--- convertor.py
+++ convertor.py
@@ -1,11 +1,22 @@
 # -*- coding:utf-8 -*-
 
-from emoji_pinyin_dict import first_dict
+from emoji_pinyin_dict import first_dict, all_dict
 import pypinyin
+import random
 
 
 def get_emoji(word, py):
     return first_dict[py][0] if py in first_dict else word
+
+
+def get_random_emoji(word, py):
+    return random.sample(all_dict[py], 1)[0] if py in all_dict else word
+
+
+METHOD = {
+    0: get_emoji,
+    1: get_random_emoji
+}
 
 
 def get_pinyin(word):
@@ -13,21 +24,21 @@ def get_pinyin(word):
     return pypinyin.pinyin(word)
 
 
-def get_converted(words):
+def get_converted(words, method=0):
     emojis = []
     pys = get_pinyin(words)
     for py in pys:
-        emo = get_emoji(py[0], py[0])
+        emo = METHOD[method](py[0], py[0])
         emojis.append(emo)
     return ''.join(emojis)
 
 
-def run():
-    words = raw_input(u"请输入要转换的字符：").decode('utf-8')
-    print(get_converted(words))
+def run(method=0):
+    words = raw_input("Please input text: ").decode('utf-8')
+    print(get_converted(words, method))
 
 
 if __name__ == '__main__':
-    run()
+    run(1)
 
 
