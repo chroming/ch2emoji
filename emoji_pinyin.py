bbc3212dfcb8dd0f5eae537a3e84fc8c89aef5aa
chroming <guangzhishang@gmail.com>
2017-05-09 00:26:18 +0800

    添加emoji 中文全部拼音字典

--- emoji_pinyin.py
+++ emoji_pinyin.py
@@ -4,7 +4,22 @@ from emoji_dict import emoji_dict
 import pypinyin
 
 
-py_dict = {pypinyin.pinyin(key, heteronym=True)[0][0]:values for key, values in emoji_dict.iteritems()}
-print py_dict
+def get_first_dict():
+    py_dict = {pypinyin.pinyin(key, heteronym=True)[0][0]:values for key, values in emoji_dict.iteritems()}
+    print py_dict
 
 
+def get_all_dict():
+    all_dict = {}
+    for key, values in emoji_dict.iteritems():
+        py_list = pypinyin.pinyin(key)
+        for py in py_list:
+            if py[0] in all_dict:
+                all_dict[py[0]].extend(values)
+            else:
+                all_dict[py[0]] = values
+    print(all_dict)
+
+
+if __name__ == '__main__':
+    get_all_dict()
